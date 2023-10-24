9303c34937f83e22d4e027b44e585d83680e1ea4
Olivier PAKOSZ <olivier.pakosz@epitech.eu>
2023-10-24 10:20:17 +0200

    ajout exo04

---
+++ ex_03/Gecko.java
@@ -0,0 +1,27 @@
+public class Gecko {
+    private String name;
+
+    public Gecko() {
+        this("Unknown");
+    }
+
+    public Gecko(String name) {
+        this.name = name;
+        if (name.equals("Unknown")) {
+            System.out.println("Hello!");
+        } else {
+            System.out.println("Hello " + name + "!");
+        }
+    }
+    public String getName() {
+        return name;
+    }
+   
+
+    public static void main(String[] args) {
+        Gecko arthur = new Gecko("Arthur");
+        Gecko benjy = new Gecko();
+        System.out.println(arthur.name);
+        System.out.println(benjy.name);
+    }
+}
